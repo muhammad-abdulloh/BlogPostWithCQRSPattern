ab77a42e8a050c32fadb591740c51a985dac2325
muhammad-abdulloh <clevercoderr@gmail.com>
2023-12-13 21:56:05 +0500

    Add Blazor and SignalR

---
+++ BlogPost.Tests/PostTitle.cs
@@ -0,0 +1,16 @@
+//namespace BlogPost.Tests
+//{
+//    public class PostTitle
+//    {
+//        [Theory]
+//        [InlineData("passwod12!", "logindacay", true)]
+//        [InlineData("passwo", "logindacay", false)]
+//        public void CheckTestPostCreate(string password, string login, bool isTrue)
+//        {
+//            var qiymat =  Nimadur.LoginPasswordChecker(password, login);
+//            var result = qiymat == true ? "To'g'ri" : null;
+            
+//            Assert.True(isTrue, result);
+//        }
+//    }
+//}
